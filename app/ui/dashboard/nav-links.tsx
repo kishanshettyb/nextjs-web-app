8ada2933406a55fbd31c45109cb268e5160c8e43
kishanshettyb <kishanqr@gmail.com>
2024-03-17 18:11:58 +0530

    add emoji

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -26,18 +26,9 @@ export default function NavLinks() {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          // <a
-          //   key={link.name}
-          //   href={link.href}
-          //   className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-          // >
-          //   <LinkIcon className="w-6" />
-          //   <p className="hidden md:block">{link.name}</p>
-          // </a>
            <Link
             key={link.name}
             href={link.href}
-            
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
@@ -46,7 +37,7 @@ export default function NavLinks() {
             )}
           >
             <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
+            <p className="hidden md:block">{link.name} 😊</p>
           </Link>
         );
       })}
